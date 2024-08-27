687d7c7ec0c6b0933ed68a88d2ed545f6d5d84ae
Lucas Resende <lucasvieiraresende@gmail.com>
2024-08-27 16:12:26 -0300

    Transfered all major functionalities to 'functions.py' script and added capability to print the note played

--- Tuner.py
+++ Tuner.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pyaudio
 import matplotlib.pyplot as plt
-from functions import load_notes_frequencies_csv, listen_pyaudio
+from functions import load_notes_frequencies_csv, listen_pyaudio, plot_raw_signal, get_signal_spectrum, plot_spectrum, note_frequency, match_note
+
+#%%
+plotting = True
 
 #%% Load the data from the CSV file
 all_notes, note_list = load_notes_frequencies_csv()
@@ -9,58 +12,45 @@ all_notes, note_list = load_notes_frequencies_csv()
 #%% Listen with PyAudio
 
 # Parameters
-CHUNK = 1024
-RATE = 44100
-RECORD_SECONDS = 2
+RECORD_SECONDS = 1
 
 # Initialize pyaudio
 audio = pyaudio.PyAudio()
 
 # Retrieve audio data
-audio_data = listen_pyaudio(audio, CHUNK, RATE, RECORD_SECONDS)
+audio_data = listen_pyaudio(audio, RECORD_SECONDS)
+audio.terminate()
 
 #%% Plot data
-plt.figure(figsize=(10, 4))
-plt.plot(audio_data)
-plt.title('Recorded Audio Signal')
-plt.xlabel('Sample Index')
-plt.ylabel('Amplitude')
-volume_threshold = 100
+if plotting:
+    plot_raw_signal(audio_data)
 
 #%%
+volume_threshold = 100
+
 if np.mean(np.abs(audio_data)) > volume_threshold:
-    audio_data = audio_data / np.max(np.abs(audio_data)) # normalize audio_data
+    normalized_audio_data = audio_data / np.max(np.abs(audio_data)) # normalize audio_data
     
-    # Perform FFT
-    n = len(audio_data)
-    audio_data_fft = np.abs(np.fft.fft(audio_data))
-    frequencies = np.fft.fftfreq(n, 1/RATE)
-    audio_fft = audio_data_fft[0:n//2]
-    freq = frequencies[0:n//2]
+    # Perform FFT and get signal spectrum
+    x, f = get_signal_spectrum(normalized_audio_data)
     
-    # Trim signal
-    aux = freq <= 2500
-    f = freq[aux]
-    x = audio_fft[aux]
+    # Identify note frequency
+    magnitude_threshold = 500 # threshold for selecting first harmonic
+    note = note_frequency(x, f, magnitude_threshold)
     
-    # Plot
-    plt.figure(figsize=(10, 4))
-    plt.plot(f, x)
-    plt.title('Recorded Audio Signal Spectrum')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Magnitude')
+    # Plot spectrum
+    if plotting:
+        plot_spectrum(x, f, magnitude_threshold, note)
+        
+    # Identify note closest to first harmonic
+    output_str = match_note(note, all_notes, note_list)
+    print(output_str)
     
-    # Identify harmonics
-    threshold = 500
-    ft = f[x >= threshold]
-    print(ft)
-    plt.hlines(threshold, xmin=0, xmax=f[-1], colors='red')
-    
-    # Identify first harmonic
-    note = np.mean(ft[ft < ft[0]*1.05])
-    plt.vlines(note, ymin=0, ymax=np.max(x), colors='red')
+        
     
     
     
     
-plt.show()
+    
+if plotting: 
+    plt.show()
--- functions.py
+++ functions.py
@@ -11,8 +11,10 @@ def load_notes_frequencies_csv():
     return all_notes, note_list
 
 
-def listen_pyaudio(audio, CHUNK, RATE, RECORD_SECONDS):
+def listen_pyaudio(audio, RECORD_SECONDS):
     FORMAT = pyaudio.paInt16
+    CHUNK = 1024
+    RATE = 44100
     CHANNELS = 1
 
     # Open stream
@@ -34,8 +36,102 @@ def listen_pyaudio(audio, CHUNK, RATE, RECORD_SECONDS):
     # Close stream
     stream.stop_stream()
     stream.close()
-    audio.terminate()
     
     audio_data = np.concatenate(frames) # convert frames to single numpy array
     return audio_data
+
+
+def plot_raw_signal(audio_data):
+    plt.figure(figsize=(10, 4))
+    plt.plot(audio_data)
+    plt.title('Recorded Audio Signal')
+    plt.xlabel('Sample Index')
+    plt.ylabel('Amplitude')
+    plt.xlim((0, len(audio_data)))
     
+   
+def get_signal_spectrum(audio_data):
+    # Perform FFT
+    n = len(audio_data)
+    audio_data_fft = np.abs(np.fft.fft(audio_data))
+    RATE = 44100
+    frequencies = np.fft.fftfreq(n, 1/RATE)
+    audio_fft = audio_data_fft[0:n//2]
+    freq = frequencies[0:n//2]
+    
+    # Trim signal
+    aux = freq <= 2500
+    f = freq[aux]
+    x = audio_fft[aux]
+    
+    return x, f
+
+
+def plot_spectrum(x, f, threshold, note):
+    plt.figure(figsize=(10, 4))
+    plt.plot(f, x)
+    plt.title('Recorded Audio Signal Spectrum')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.hlines(threshold, xmin=0, xmax=f[-1], colors='red')
+    if note:
+        plt.vlines(note, ymin=0, ymax=np.max(x)*1.1, colors='red')
+    plt.xlim((0, f[-1]))
+    plt.ylim((0, np.max(x) * 1.1))
+    
+    
+def note_frequency(x, f, threshold):
+    if max(x) > threshold:
+        # Identify harmonics
+        ft = f[x >= threshold]
+        
+        # Identify first harmonic
+        note = np.mean(ft[ft < ft[0]*1.05])
+    else:
+        note = None
+    
+    return note
+
+
+def match_note(note, all_notes, note_list):
+    # Identify note closest to first harmonic
+    ratios = note/all_notes
+    closeness = np.abs(ratios - 1)
+    indices = np.where(closeness == np.min(closeness))
+    
+    note_str = note_list[indices[0][0]]
+    
+    # Detect whether note is higher or lower then target
+    difference = (ratios[indices] - 1) * 100
+    if difference > 2:
+        difference_str = " ↓↓↓"
+    elif difference > 1.3:
+        difference_str = " ↓↓"
+    elif difference > 0.5:
+        difference_str = " ↓"
+    elif difference < 0.5 and difference > -0.5:
+        difference_str = ""
+    elif difference < -2:
+        difference_str = " ↑↑↑"
+    elif difference < -1.3:
+        difference_str = " ↑↑"
+    else:
+        difference_str = "↑"
+        
+    return note_str + difference_str
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
